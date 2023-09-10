c6ac7fad5968c4ae3bea02cae9034f8b3303cde8
Ramkumar R <indianairforceall@gmail.com>
2023-09-11 01:42:40 +0530

    tailwind implimented

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [],
+  theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#292524',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  plugins: [],
+} satisfies Config
+
